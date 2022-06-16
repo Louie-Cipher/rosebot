611878929a935befb1cb8e38037a7f54fb3e452b
Louie-Cipher <louie.spena@outlook.com>
2022-06-15 23:54:05 -0300

    live announcing improvements

--- src/utils/liveAnnouncing.ts
+++ src/utils/liveAnnouncing.ts
@@ -10,9 +10,27 @@ const buttons = new MessageActionRow()
     .setComponents([
         new MessageButton()
             .setLabel('Assistir na twitch')
+            .setEmoji('<:anya_pointing:982294755260104734>')
             .setURL('https://www.twitch.tv/roselicia')
             .setStyle('LINK')
     ]);
+
+const phrases = [
+    'Vá assistir e aproveitar a stream!',
+    'Clique abaixo para acompanhar a live!',
+    'Vá acompanhar e comentar na twitch!',
+    'Junte-se na twitch e aproveite a live!',
+    'Entre na twitch e aproveite a stream!',
+];
+
+const emoticons = [
+    ':3',
+    'OwO',
+    'UwU',
+    '<:uwucoin:978022888470904832>',
+    '<:zerowuw:866481399832903700>',
+    '<a:rainbow_hype:866480734834130945'
+];
 
 export { isLive };
 
@@ -35,12 +53,15 @@ export default async function liveAnnouncing(newPresence: Presence, oldPresence?
 
         const liveThumbnail = liveActivity.assets?.largeImageURL() || liveActivity.assets?.smallImageURL();
 
+        const randPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+        const randEmoticon = emoticons[Math.floor(Math.random() * emoticons.length)];
+
         let announcingEmbed = new MessageEmbed()
             .setColor('PURPLE')
             .setTitle('<a:twitch_a:978802763855200287> Rose está ao vivo :3 <a:twitch_a:978802763855200287>')
             .setDescription(`Jogando **${liveActivity.state}**\n` +
                 `"${liveActivity.details}"\n\n` +
-                `Vá assistir e aproveitar a stream! OwO`)
+                `${randPhrase} ${randEmoticon}`)
 
         if (liveThumbnail && liveThumbnail != null) announcingEmbed.setThumbnail(liveThumbnail);
 
