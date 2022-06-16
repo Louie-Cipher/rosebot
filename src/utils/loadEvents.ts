7ee921c13ee0a8eeeb6ab02655a4792002552405
Louie-Cipher <louie.spena@outlook.com>
2022-06-15 23:53:48 -0300

    log events count

--- src/utils/loadEvents.ts
+++ src/utils/loadEvents.ts
@@ -1,6 +1,8 @@
 import { readdirSync } from 'fs';
+import { consoleLog } from '.';
 
 export function loadEvents() {
     const files = readdirSync(`${__dirname}/../events`).filter(file => file.endsWith('.js') || file.endsWith('.ts'));
     files.forEach(file => import(`../events/${file}`));
+    consoleLog(`[EVENTS_LOADER] ${files.length} Events loaded!`);
 }
